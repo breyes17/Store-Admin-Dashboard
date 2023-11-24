fee669d223fa222a7d9c801f2e5c53694b5546c9
Bryan Reyes <bryan.reyes@fidx.co.jp>
2023-11-24 15:06:21 +0800

    Move the nav to page.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { ClerkProvider, UserButton } from '@clerk/nextjs'
+import { ClerkProvider } from '@clerk/nextjs'
 
 import './globals.css'
 
@@ -20,10 +20,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <nav>
-            <UserButton afterSignOutUrl="/"/>
-          </nav>
-          {children}
+            {children}
         </body>
       </html>
     </ClerkProvider>
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import { Button } from '@/components/ui/button';
+import { UserButton } from '@clerk/nextjs';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <nav className='p-4'>
+        <UserButton afterSignOutUrl="/"/>
+      </nav>
       <Button>Testing</Button>
     </main>
   );
